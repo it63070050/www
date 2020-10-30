b5aee2b5fb2af1c15bc0c37b836fbc4a073ccfe2
it63070050 <73683228+it63070050@users.noreply.github.com>
2020-10-30 12:49:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <html>
     <head>
-        <title>Hello world</title>
+        <meta charset="UTF-8">
     </head>
     <body>
-        <h1>My First Heading</h1>
-        <p>My First Paragraph</p>
+        <h1>ชื่อ : นางสาวณัฐณิชา นามสกุล : โล่ห์ชาวนา</h1>
+        <p>ชื่อเล่น : อั้ม</p>
         <img src="t1.jpg">
+        <img src="t1_2.png">
         <br>
-        <a href="https://www.gamingdose.com/wp-content/uploads/2019/02/84841.jpg">This is a Link</a>
-
+        <a href="https://t1.gg/">This is a Link</a>
+        
         <h2>The unordered list</h2>
         <ul>
             <li>Google</li>
